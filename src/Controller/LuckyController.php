82e28b9999d64ca3d37757b741e7d345b90beb8b
zeroyukiy <zeroyukiy@hotmail.com>
2021-07-22 11:06:35 +0200

    first commit

---
+++ src/Controller/LuckyController.php
@@ -0,0 +1,32 @@
+<?php
+// src/Controller/LuckyController.php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LuckyController
+{
+
+    /**
+     * @Route("/")
+     */
+    public function index(): Response
+    {
+        return new Response(
+            '<html><body>Home Page</body><html>'
+        );
+    }
+
+    /**
+     * @Route("/number")
+     */
+    public function number(): Response
+    {
+        $number = random_int(0, 100);
+
+        return new Response(
+            '<html><body>Lucky number: ' . $number . '</body></html>'
+        );
+    }
+}
